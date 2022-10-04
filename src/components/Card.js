5ebe77b559495647d20b126c60f5a6b8eb08430a
binhussen <mhdhsna@gmail.com>
2022-10-04 21:33:02 +0300

    Add Card

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+const card = () => {
+  return (
+    <div className="m-1 h-25vh w-47vw p-6 cursor-pointer rounded bg-slate-500">
+      <h3 className="text-xl font-medium mx-1">Ethiopia</h3>
+      <p>Region : Africa</p>
+      <p>Population: 114,0000</p>
+    </div>
+  )
+}
+
+export default card;

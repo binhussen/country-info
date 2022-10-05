f6ccca9510041b995691ab1c80dae8c4c8d566e9
binhussen <mhdhsna@gmail.com>
2022-10-05 20:26:07 +0300

    Filter by continent

--- src/redux/countries.js
+++ src/redux/countries.js
@@ -7,6 +7,7 @@ const API_URL = 'https://restcountries.com/v3.1';
 
 const GET_COUNTRIES = 'weather/countries/GET_COUNTRIES';
 const SEARCH_COUNTRIES = 'weather/countries/SOURCE_COUNTRIES';
+const FILTER_COUNTRIES = 'weather/countries/FILTER_COUNTRIES';
 
 const initialCountries = [];
 
@@ -16,13 +17,15 @@ export default function countryReducer(state = initialCountries, action) {
       return [...action.payload];
     case `${SEARCH_COUNTRIES}/fulfilled`:
       return [...action.payload];
+    case `${FILTER_COUNTRIES}/fulfilled`:
+      return [...action.payload];
     default:
       return state;
   }
 }
 
-export const getCountries = createAsyncThunk(GET_COUNTRIES, async (country) => {
-  const response = await axios.get(`${API_URL}/name/${country}`);
+export const getCountries = createAsyncThunk(GET_COUNTRIES, async () => {
+  const response = await axios.get(`${API_URL}/all`);
   const countries = response.data.map((country) => ({
     name: country.name,
     code: country.cca2,
@@ -60,3 +63,24 @@ export const searchCountries = createAsyncThunk(SEARCH_COUNTRIES, async (name) =
   }));
   return countries;
 });
+
+export const filterCountries = createAsyncThunk(FILTER_COUNTRIES,
+  async (continent) => {
+    const response = await axios.get(`${API_URL}/region/${continent}`);
+    const countries = response.data.map((country) => ({
+      name: country.name,
+      code: country.cca2,
+      currency: country.currencies,
+      capital: country.capital,
+      region: country.region,
+      subregion: country.subregion,
+      languages: country.languages,
+      borders: country.borders,
+      area: country.area,
+      map: country.maps,
+      population: country.population,
+      timezones: country.timezones,
+      flag: country.flags,
+    }));
+    return countries;
+  });

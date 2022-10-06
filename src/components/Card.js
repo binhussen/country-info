f3263a30877364c020be5ba3aa6221744f42d5c7
binhussen <mhdhsna@gmail.com>
2022-10-06 09:16:32 +0300

    Display Countries

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,37 @@
-const Card = () => (
-  <a href="/public">
-    <div className="flex items-end flex-col justify-between p-4 shadow-lg h-25vh bg-slate-500 text-white">
-      <svg
-        stroke="currentColor"
-        fill="none"
-        strokeWidth="0"
-        viewBox="0 0 24 24"
-        height="1em"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M12.0519 14.8285L13.4661 16.2427L17.7088 12L13.4661 7.7574L12.0519 9.17161L13.8804 11H6.34321V13H13.8803L12.0519 14.8285Z"
-          fill="currentColor"
-        />
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M19.7782 19.7782C24.0739 15.4824 24.0739 8.51759 19.7782 4.22183C15.4824 -0.0739417 8.51759 -0.0739417 4.22183 4.22183C-0.0739417 8.51759 -0.0739417 15.4824 4.22183 19.7782C8.51759 24.0739 15.4824 24.0739 19.7782 19.7782ZM18.364 18.364C21.8787 14.8492 21.8787 9.15076 18.364 5.63604C14.8492 2.12132 9.15076 2.12132 5.63604 5.63604C2.12132 9.15076 2.12132 14.8492 5.63604 18.364C9.15076 21.8787 14.8492 21.8787 18.364 18.364Z"
-          fill="currentColor"
-        />
-      </svg>
-      <div className="text-right uppercase">
-        <h3 className="text-xl font-medium mx-1">Ethiopia</h3>
-        <p>934</p>
-      </div>
-    </div>
-  </a>
-);
+const Card = ({ country }) => {
+  const { code } = country;
+
+  return (
+		<a
+			className="text-white transition-shadow opacity-100 bg-slate-500"
+			href="/public">
+			<div className="flex items-end flex-col justify-between p-4 shadow-2xl h-25vh">
+				<svg
+					stroke="currentColor"
+					fill="none"
+					strokeWidth="0"
+					viewBox="0 0 24 24"
+					height="1em"
+					width="1em"
+					xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M12.0519 14.8285L13.4661 16.2427L17.7088 12L13.4661 7.7574L12.0519 9.17161L13.8804 11H6.34321V13H13.8803L12.0519 14.8285Z"
+						fill="currentColor"
+					/>
+					<path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="M19.7782 19.7782C24.0739 15.4824 24.0739 8.51759 19.7782 4.22183C15.4824 -0.0739417 8.51759 -0.0739417 4.22183 4.22183C-0.0739417 8.51759 -0.0739417 15.4824 4.22183 19.7782C8.51759 24.0739 15.4824 24.0739 19.7782 19.7782ZM18.364 18.364C21.8787 14.8492 21.8787 9.15076 18.364 5.63604C14.8492 2.12132 9.15076 2.12132 5.63604 5.63604C2.12132 9.15076 2.12132 14.8492 5.63604 18.364C9.15076 21.8787 14.8492 21.8787 18.364 18.364Z"
+						fill="currentColor"
+					/>
+				</svg>
+				<div className="text-right uppercase">
+					<h3 className="text-xl font-medium mx-1">{code}</h3>
+					<p>934</p>
+				</div>
+			</div>
+		</a>
+	);
+};
 
 export default Card;
---
+++ src/pages/Country.js
@@ -0,0 +1,38 @@
+/** @format */
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Card from '../components/Card';
+import Filter from '../components/Filter';
+import { getCountries } from '../redux/countries';
+
+/* <div
+className="border-x-slate-400 rounded-full mx-auto animate-spin  w-24 h-24 my-24"
+/> */
+
+const Country = () => {
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries);
+
+  useEffect(() => {
+    dispatch(getCountries());
+  }, []);
+
+  return (
+		<section className="min-h-screen">
+			<div className="bg-slate-500 text-white p-3 uppercase flex justify-between">
+				<h2>World Countries</h2>
+					<Filter />
+			</div>
+			<div className="grid grid-cols-2 justify-center">
+				{countries.map((country) => (
+					<Card
+						key={country.code}
+						country={country}
+					/>
+				))}
+			</div>
+		</section>
+	);
+};
+export default Country;
